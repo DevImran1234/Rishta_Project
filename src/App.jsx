cd884a7cfffd4009948e9c156bf7834b56a993bb
Imran123455899 <i52087174@gmail.com>
2024-08-04 21:53:27 +0500

    Your commit message

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from './Components/Home/Home';
 import Login from './Components/Login/Login';
 import Signup from './Components/Signup/Signup';
 import MarriageForm from './Components/MarriageForm/MarriageForm';
-import Users from './Components/Users';
+import Users from './Components/Users/Users'
 const App = () => {
   return (
     <Router>
