9fc6ddeb1c3602501a06166d85d03ec5ea472f48
Imran123455899 <i52087174@gmail.com>
2024-08-04 14:38:02 +0500

    your commit change

--- src/Components/Advices/Relationship.jsx
+++ src/Components/Advices/Relationship.jsx
@@ -3,18 +3,18 @@ import img from "../../images/img2.jpg";
 import img2 from "../../images/img6.webp";
 import img3 from "../../images/img4.webp";
 import "./Relationship.css";
+
 const Relationship = () => {
   return (
-     <div className="bg">
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-5 max-[768px]:gap-[80px] mt-10 ml-90">
-      {/* First Card */}
-      <div className="relative">
-        <div className="bg-white rounded-[16px] shadow-md px-6 py-12 h-[100vh]">
-          <h2 className="font-romanesco text-[#2F82A0] text-4xl md:text-5xl -top-10 md:top-[-50px] w-[93%] text-center md:text-center md:-top-[-55px] absolute">
+    <div className="bg">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-5 max-[768px]:gap-[80px] mt-10">
+        {/* First Card */}
+        <div className="flex flex-col bg-white rounded-[16px] shadow-md px-6 py-12">
+          <h2 className="font-romanesco text-[#2F82A0] text-4xl md:text-5xl mb-6 text-center">
             Relationship Advice
           </h2>
-          <img src={img} alt="Ring on book" className="w-full pb-12 md:pb-6" />
-          <p className="mt-2 text-gray-600">
+          <img src={img} alt="Ring on book" className="w-full mb-6" />
+          <p className="text-gray-600">
             Here we have experts who work hard to write blogs on relationships,
             marriage, dating, and finding love, giving you vista and motivation
             to have a great love life. Following these tips can help you in your
@@ -25,16 +25,14 @@ const Relationship = () => {
             </a>
           </p>
         </div>
-      </div>
 
-      {/* Second Card */}
-      <div className="relative ml-90">
-        <div className="bg-white rounded-[16px] shadow-md px-6 py-12 h-[100vh]">
-          <h2 className="font-romanesco text-[#2F82A0] text-4xl md:text-5xl -top-10 md:top-[-50px] w-[93%] text-center md:text-center md:-top-[-55px] absolute">
+        {/* Second Card */}
+        <div className="flex flex-col bg-white rounded-[16px] shadow-md px-6 py-12">
+          <h2 className="font-romanesco text-[#2F82A0] text-4xl md:text-5xl mb-6 text-center">
             Find someone special near you
           </h2>
-          <img src={img2} alt="Couple" className="w-full pb-12 md:pb-6" />
-          <p className="mt-2 text-gray-600">
+          <img src={img2} alt="Couple" className="w-full mb-6" />
+          <p className="text-gray-600">
             Connect with your soulmate that can be nearby or within your city.
             Our members are committed to looking for long-term healthy
             relationships. This platform will help you to find love and give a
@@ -44,20 +42,18 @@ const Relationship = () => {
             </a>
           </p>
         </div>
-      </div>
 
-      {/* Third Card */}
-      <div className="relative">
-        <div className="bg-white rounded-[16px] shadow-md px-6 py-12 h-[100vh]">
-          <h2 className="font-romanesco text-[#2F82A0] text-4xl md:text-5xl -top-10 md:top-[-50px] w-[93%] text-center md:text-center md:-top-[-55px] absolute">
-            Let a matchmaker help you{" "}
+        {/* Third Card */}
+        <div className="flex flex-col bg-white rounded-[16px] shadow-md px-6 py-12">
+          <h2 className="font-romanesco text-[#2F82A0] text-4xl md:text-5xl mb-6 text-center">
+            Let a matchmaker help you
           </h2>
-          <img src={img3} alt="img" className="w-full pb-12 md:pb-6" />
-          <p className="mt-2 text-gray-600">
-            If you are a busy professionals then it’s time to think about a
+          <img src={img3} alt="img" className="w-full mb-6" />
+          <p className="text-gray-600">
+            If you are a busy professional, then it’s time to think about a
             matchmaker who can help you. There are various reasons to take help
             from a matchmaker.
-            <ul className="list-disc list-inside mt-2 list-red">
+            <ul className="list-disc list-inside mt-2">
               <li>Discreet and privacy</li>
               <li>Saves time and safety</li>
               <li>Personalized service</li>
@@ -72,7 +68,6 @@ const Relationship = () => {
           </p>
         </div>
       </div>
-    </div>
     </div>
   );
 };
