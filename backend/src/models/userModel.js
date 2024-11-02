7385b90078614fabd3ae2b5b1a59f77580528c35
8ibhj <moazzam@digitaldiraction.com>
2024-11-02 22:49:08 +0500

    add role

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -7,7 +7,7 @@ const userSchema = new Schema(
     name: { type: String, required: true },
     email: { type: String, unique: true, required: true },
     password: { type: String, required: true },
-    role: { type: String, enum: ["user", "consultant"], required: true },
+    role: { type: String, enum: ["user", "consultant"] },
     isVerified: { type: Boolean, default: false },
     verificationToken: String,
     companyName: {
