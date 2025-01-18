e9cac45d0e7cf67b8287c4a35a93413cc186d2e3
Your Name <you@example.com>
2025-01-19 01:57:04 +0500

    profile apis

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,9 +1,12 @@
 import express from "express";
 import userRoutes from "./userRoutes.js";
+import profileRoutes from "./profileRoute.js";
 
 const router = express.Router();
 
 router.use("/users", userRoutes);
+
+router.use("/profile", profileRoutes);
 
 
 export default router;
